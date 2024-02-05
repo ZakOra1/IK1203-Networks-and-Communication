5c95f748754efa42d3976f428fd612e3906c9dcf
ZakOra1 <32168076+ZakOra1@users.noreply.github.com>
2024-02-05 13:11:15 +0100

    Solved problem echotest

--- Task 1 - TCPAsk/tcpclient/TCPClient.java
+++ Task 1 - TCPAsk/tcpclient/TCPClient.java
@@ -7,48 +7,22 @@ public class TCPClient {
     public TCPClient() {
     
     }
-    /*public byte[] askServer(String hostname, int port) throws IOException {
-        
-        //Open connection with server
-        int length = 0;
-        Socket socket = new Socket(hostname, port);
-        byte[] serverResponse = new byte[512];
-        ByteArrayOutputStream response = new ByteArrayOutputStream();
-        
-        InputStream inputStream = socket.getInputStream();
-
-        //Server responds
-        while(socket.getInputStream().read() != -1) {
-            length = inputStream.read(serverResponse);
-            response.write(serverResponse, 0, length);
-        }
-        
-        socket.close();
-
-        return response.toByteArray();
-    }*/
 
     public byte[] askServer(String hostname, int port, byte[] toServerBytes) throws IOException {
-        
-        //Open connection with server
         Socket socket = new Socket(hostname, port);
-        byte[] serverResponse = new byte[512];
         ByteArrayOutputStream response = new ByteArrayOutputStream();
-        
-        //Contact server
-        socket.getOutputStream().flush();
-        socket.getOutputStream().write(toServerBytes);
-        
-        InputStream inputStream = socket.getInputStream();
-        int length = inputStream.read(serverResponse);
 
-        //Server responds
-        while(length != -1) {
-            //response.write(serverResponse);
+        // Contact server
+        socket.getOutputStream().write(toServerBytes);
+
+        InputStream inputStream = socket.getInputStream();
+        byte[] serverResponse = new byte[512]; // Moved inside the loop
+        int length;
+
+        while ((length = inputStream.read(serverResponse)) != -1) {
             response.write(serverResponse, 0, length);
-            length = inputStream.read(serverResponse);
         }
-        
+
         socket.close();
 
         return response.toByteArray();
