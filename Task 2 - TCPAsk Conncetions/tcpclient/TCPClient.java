8adf1540656e198e1268e27548cab41443b47ce1
ZakOra1 <32168076+ZakOra1@users.noreply.github.com>
2024-02-18 18:54:22 +0100

    Initial try on task 2

--- Task 2 - TCPAsk Conncetions/tcpclient/TCPClient.java
+++ Task 2 - TCPAsk Conncetions/tcpclient/TCPClient.java
@@ -3,14 +3,32 @@ import java.net.*;
 import java.io.*;
 
 public class TCPClient {
-    
+    //  Shutdown controls if the client closes the connection
+    //  Timeout decides maximum time (in ms) to wait for new data
+    //  Limit controls maximum data (in bytes) to recive before returning
+
+    private boolean shutdown;
+    private Integer timeout, limit;
+
+    public TCPClient(boolean shutdown, Integer timeout, Integer limit) {
+        this.shutdown = shutdown;
+        this.timeout = timeout;
+        this.limit = limit;
+
+    }
+
     public TCPClient() {
-    
+
     }
 
     public byte[] askServer(String hostname, int port, byte[] toServerBytes) throws IOException {
         Socket socket = new Socket(hostname, port);
         ByteArrayOutputStream response = new ByteArrayOutputStream();
+
+        // Set the socket timeout if specified
+        if (timeout != null) {
+            socket.setSoTimeout(timeout);
+        }
 
         // Contact server
         socket.getOutputStream().write(toServerBytes);
@@ -18,9 +36,25 @@ public class TCPClient {
         InputStream inputStream = socket.getInputStream();
         byte[] serverResponse = new byte[512]; // Moved inside the loop
         int length;
+        int totalBytesRead = 0;
 
-        while ((length = inputStream.read(serverResponse)) != -1) {
-            response.write(serverResponse, 0, length);
+        try {
+            while ((length = inputStream.read(serverResponse)) != -1) {
+                response.write(serverResponse, 0, length);
+                totalBytesRead += length;
+
+                if (limit != null && totalBytesRead >= limit) {
+                    break;
+                }
+            }
+        } catch (SocketTimeoutException e) {
+            // Handle socket timeout
+            System.err.println("Socket timeout occurred.");
+        }
+
+        // Shutdown the outgoing connection if specified
+        if (shutdown) {
+            socket.shutdownOutput();
         }
 
         socket.close();
